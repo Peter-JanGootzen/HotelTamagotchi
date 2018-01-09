7cbe71a81afe3b5fcbfaff2f7b05599132ac5872
Bram-Boris Meerlo <bbmeerlo@avans.nl>
2018-01-09 11:17:07 +0000

    moved dbcontext and model to web project

---
+++ HotelTamagotchi.Web/Models/HotelRoomType.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelTamagotchi.Model
+{
+    public enum HotelRoomType : byte
+    {
+        Restroom = 0,
+        Gameroom = 1,
+        Workroom = 2,
+        Fightroom = 3
+    }
+}
---
+++ HotelTamagotchi.Web/Models/HotelTamagotchiEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace HotelTamagotchi.Web.Models
+{
+    public partial class HotelTamagotchiEntities : DbContext
+    {
+        public HotelTamagotchiEntities()
+            : base("name=HotelTamagotchiEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<HotelRoom> HotelRooms { get; set; }
+        public virtual DbSet<Tamagotchi> Tamagotchis { get; set; }
+    }
+}
