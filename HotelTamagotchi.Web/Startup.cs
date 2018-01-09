0816fd98d96ae0d9942de4ef22fa0758851409c9
Peter-JanGootzen <PeterJan.Gootzen@gmail.com>
2018-01-09 10:49:06 +0100

    Added Web and Test project

---
+++ HotelTamagotchi.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelTamagotchi.Web.Startup))]
+namespace HotelTamagotchi.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
