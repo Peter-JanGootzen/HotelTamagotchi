08e03c8fe2ddb23a929d5e98033b37dc5ed11da3
Peter-JanGootzen <PeterJan.Gootzen@gmail.com>
2018-01-09 15:35:11 +0100

    Added HotelRoom CRUD and implemented with repo

---
+++ HotelTamagotchi.Web/Controllers/HotelRoomController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HotelTamagotchi.Web.Models;
+using HotelTamagotchi.Web.Repositories;
+
+namespace HotelTamagotchi.Web.Controllers
+{
+    public class HotelRoomController : Controller
+    {
+        private IRepository<HotelRoom> HotelRoomRepo;
+
+        public HotelRoomController(IRepository<HotelRoom> hotelRoomRepository)
+        {
+            HotelRoomRepo = hotelRoomRepository;
+        }
+
+        public HotelRoomController()
+        {
+            HotelRoomRepo = new HotelRoomRepository();
+        }
+
+        // GET: HotelRoom
+        public ActionResult Index()
+        {
+            return View(HotelRoomRepo.GetAll());
+        }
+
+        // GET: HotelRoom/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HotelRoom hotelRoom = HotelRoomRepo.Find(id);
+            if (hotelRoom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotelRoom);
+        }
+
+        // GET: HotelRoom/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HotelRoom/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Size,Type")] HotelRoom hotelRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                HotelRoomRepo.Add(hotelRoom);
+                return RedirectToAction("Index");
+            }
+
+            return View(hotelRoom);
+        }
+
+        // GET: HotelRoom/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HotelRoom hotelRoom = HotelRoomRepo.Find(id);
+            if (hotelRoom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotelRoom);
+        }
+
+        // POST: HotelRoom/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Size,Type")] HotelRoom hotelRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                HotelRoomRepo.SetChanged(hotelRoom);
+                return RedirectToAction("Index");
+            }
+            return View(hotelRoom);
+        }
+
+        // GET: HotelRoom/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HotelRoom hotelRoom = HotelRoomRepo.Find(id);
+            if (hotelRoom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotelRoom);
+        }
+
+        // POST: HotelRoom/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HotelRoom hotelRoom = HotelRoomRepo.Find(id);
+            HotelRoomRepo.Remove(hotelRoom);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                HotelRoomRepo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
