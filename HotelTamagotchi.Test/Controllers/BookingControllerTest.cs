0c596b6c64d7ccc22a18d4f745ebda397bb525df
Bram-Boris Meerlo <bbmeerlo@avans.nl>
2018-01-12 11:56:56 +0100

    Added booking controller test

--- HotelTamagotchi.Test/Controllers/BookingControllerTest.cs
+++ HotelTamagotchi.Test/Controllers/BookingControllerTest.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HotelTamagotchi.Web.Controllers;
 using HotelTamagotchi.Web.Models;
 using HotelTamagotchi.Web.Repositories;
+using HotelTamagotchi.Web.ViewModels;
+using System.Web.Mvc;
 
 namespace HotelTamagotchi.Test.Controllers
 {
@@ -10,16 +12,37 @@ namespace HotelTamagotchi.Test.Controllers
     public class BookingControllerTest
     {
         [TestMethod]
-        public void BookingController_Edit()
+        public void BookingController_CreateBooking()
         {
             HotelTamagotchiEntities d = new HotelTamagotchiEntities();
             IHotelRoomRepository hr = new HotelRoomRepository(d);
             ITamagotchiRepository tr = new TamagotchiRepository(d);
             BookingController bc = new BookingController(hr, tr);
-            Tamagotchi t = new Tamagotchi()
+            FormCollection fc = new FormCollection();
+            TamagotchiViewModel t = new TamagotchiViewModel()
             {
-                Id = 1
+                Name = "Test",
+                Pennies = 100,
+                Age = 12,
+                Level = 100,
+                Health = 4,
+                Boredom = 12,
+                Alive = true
+                
             };
+            tr.Add(t);
+            HotelRoomViewModel h = new HotelRoomViewModel()
+            {
+                Size = 5,
+                Type = HotelRoomType.Fightroom
+            };
+            hr.Add(h);
+            fc.Add("1", "true,false");
+            bc.Create(fc, h);
+            Assert.AreEqual(t.ToModel().HotelRoomId, tr.Find(t.Id).HotelRoomId);
+            tr.Remove(t);
+            hr.Remove(h);
+
             
         }
     }
